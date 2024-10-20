4bea1e984f3fa7fb4a79c77d9e0c0593e88db873
pratham40 <122166099+pratham40@users.noreply.github.com>
2024-10-20 17:39:11 +0530

    set up client

---
+++ Client/src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import HomeLayout from './Layouts/HomeLayout';
+
+const App = () => (
+  <Router>
+    <HomeLayout />
+  </Router>
+);
+
+export default App;
