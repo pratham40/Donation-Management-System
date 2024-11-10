5b7417c28063168bf16f84dfbddefe0f1d242f58
pratham40 <122166099+pratham40@users.noreply.github.com>
2024-11-10 13:15:15 +0530

    add mongo cloud

--- Server/config/dbConnection.js
+++ Server/config/dbConnection.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const {connection} = await mongoose.connect(
-            'mongodb://localhost:27017/DonationManagementSystem',
-        );
-
+            process.env.MONGO_URI || 'mongodb://localhost:27017/DonationManagementSystem',
+        ); 
+        
         console.log(`MongoDB Connected: ${connection.host}`);
     } catch (err) {
         console.error(`Error: ${err.message}`);
